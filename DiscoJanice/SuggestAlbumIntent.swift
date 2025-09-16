b3cb7e18d44d7492ca34aa64b6d46eedae6f9e96
Adam Anklewicz <adam@anklewicz.com>
2025-09-16 17:49:09 -0400

    Squishing bugs

--- DiscoJanice/SuggestAlbumIntent.swift
+++ DiscoJanice/SuggestAlbumIntent.swift
@@ -1,31 +1,29 @@
 import AppIntents
 import Foundation
 
-struct SuggestAlbumIntent: AppIntent, Predictable {
+struct SuggestAlbumIntent: AppIntent {
+    typealias Output = String
     static var title: LocalizedStringResource = "Suggest an Album"
     static var description: IntentDescription = "Suggest a random album from your Discogs collection."
     
     @Parameter(title: "Discogs Username")
-    var username: String? = nil
+    var username: String?
     
     static var suggestedInvocationPhrase: String? = "Suggest an album"
     
-    func perform() async throws -> some IntentResult {
+    func perform() async throws -> some IntentResult & ReturnsValue<String> {
         let resolvedUsername: String
         if let username = username, !username.isEmpty {
             resolvedUsername = username
         } else if let storedUsername = UserDefaults.standard.string(forKey: "DiscogsUsername"), !storedUsername.isEmpty {
             resolvedUsername = storedUsername
         } else {
-            return .result(dialog: .init("Please set your Discogs username in the app settings."))
+            return .result(value: "Please set your Discogs username in the app settings.")
         }
-        
+
         let album = try await AlbumSuggestionService().suggestRandomAlbum(for: resolvedUsername)
         let responseString = "How about '\(album.title)' by \(album.artist)?"
-        return .result(value: responseString, dialog: .init(responseString))
+        return .result(value: responseString)
     }
 }
 
-extension SuggestAlbumIntent: ProvidesDialog {
-    static var dialog: IntentDialog? { nil }
-}
